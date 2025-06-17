4db547b620f0b940416b32db961a5962bfceb2da
iFaustoo <faustoordoriguez10@gmail.com>
2025-06-17 19:40:49 -0300

    -

--- guia7.4/Ej2/Properties/AssemblyInfo.cs
+++ guia7.4/Ej2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Ej2")]
+[assembly: AssemblyTitle("Ejj2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ej2")]
+[assembly: AssemblyProduct("Ejj2")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("7a50d64a-f603-4700-825a-7a26b2942d76")]
+[assembly: Guid("16b8195f-88db-49f0-9ec0-d49775cec9ee")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
